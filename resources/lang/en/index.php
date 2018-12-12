f345d99af8ee014e4b9247e7ebf2371620c7915a
vodinhduy197 <vodinhduy197@gmail.com>
2018-12-12 15:04:09 +0700

    Add view

---
+++ resources/lang/en/index.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'title' => 'Laravel Quickstart - Basic',
+    'label_task' => 'Task',
+    'btn_addTask' => 'Add Task',
+    'lang' => [
+        'en' => 'English',
+        'vi' => 'Tiếng Việt',
+    ],
+
+];
