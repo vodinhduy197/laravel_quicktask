300eb8f8bba33003a756e197e22e2610604f75e6
vodinhduy197 <vodinhduy197@gmail.com>
2018-12-17 13:51:23 +0700

    Delete function

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -16,7 +16,16 @@
 
 @if(Session::has('success'))
     <div class="alert alert-success alert-dismissible fade show col col-8" role="alert">
-        <strong>{{Session::get('success')}}</strong>
+        <strong>{{ Session::get('success') }}</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
+
+@if(Session::has('fail'))
+    <div class="alert alert-danger alert-dismissible fade show col col-8" role="alert">
+        <strong>{{ Session::get('fail') }}</strong>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@
 |
 */
 
-Route::resource('/','Index');
+Route::resource('/task','Home');
